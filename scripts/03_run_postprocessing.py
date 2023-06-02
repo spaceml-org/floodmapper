ea568f582d56c2825add52be02ddcefb9c5d01d2
crpurcell <cormac.r.purcell@gmail.com>
2023-06-02 18:19:26 +1000

    Test and validate max mode.

--- scripts/03_run_postprocessing.py
+++ scripts/03_run_postprocessing.py
@@ -56,7 +56,8 @@ def do_time_aggregation(geojsons_lst, data_out_path, permanent_water_map=None,
         num_files = len(geojsons_lst)
         tq.write(f"\tPerforming temporal aggregation of {num_files} files.")
         aggregate_floodmap = \
-            postprocess.get_floodmap_post(geojsons_lst)#.to_crs('epsg:4326')
+            postprocess.get_floodmap_post(geojsons_lst,
+                                          mode="max")#.to_crs('epsg:4326')
 
         # Add the permanent water polygons
         if permanent_water_map is not None:
